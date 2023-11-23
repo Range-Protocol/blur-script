71e371b94dda21a7260650c4b9db6de3ecbbc501
0xbateman <0xbateman@rangeprotocol.com>
2023-11-23 15:34:47 +0800

    add getCurrentDebtByLien and getRefinancingAuctionRate functions

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import sys
 from web3 import Web3
 from web3.middleware import construct_sign_and_send_raw_middleware
 
-whitelisted_functions = ["refinanceAuction", "startAuction", "seize"]
+whitelisted_functions = ["refinanceAuction", "startAuction", "seize", "getCurrentDebtByLien",
+	"getRefinancingAuctionRate"]
 
 function_name = sys.argv[1]
 if function_name not in whitelisted_functions:
-    raise Exception("function not found")
+	raise Exception("function not found")
 
 pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 rpc = {"mainnet": "https://eth.llamarpc.com", "localhost": "http://127.0.0.1:8545"}
@@ -19,30 +20,40 @@ w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
 
 file_abi = open("RangeProtocolBlurVault.json")
 VAULT_ABI = json.load(file_abi)
-vault_address = "0x9eb52339B52e71B1EFD5537947e75D23b3a7719B"
+vault_address = "0x1Dbbf529D78d6507B0dd71F6c02f41138d828990"
 vault_instance = w3.eth.contract(address=vault_address, abi=VAULT_ABI)
 nonce = w3.eth.get_transaction_count(account.address)
 
 f = open(f"transaction_calldata/{function_name}.json")
 data = json.load(f)
+unsent_tx = None
+return_data = None
 
 if function_name == "refinanceAuction":
-    unsent_tx = vault_instance.functions.refinanceAuction(
-        data["lien"], data["lienId"], data["rate"]
-    )
+	unsent_tx = vault_instance.functions.refinanceAuction(
+		data["lien"], data["lienId"], data["rate"]
+	)
 elif function_name == "startAuction":
-    unsent_tx = vault_instance.functions.startAuction(data["lien"], data["lienId"])
+	unsent_tx = vault_instance.functions.startAuction(data["lien"], data["lienId"])
 elif function_name == "seize":
-    unsent_tx = vault_instance.functions.seize(data["lienPointers"])
+	unsent_tx = vault_instance.functions.seize(data["lienPointers"])
+elif function_name == "getCurrentDebtByLien":
+	return_data = vault_instance.functions.getCurrentDebtByLien(data["lien"], data["lienId"]).call()
+elif function_name == "getRefinancingAuctionRate":
+	return_data = vault_instance.functions.getRefinancingAuctionRate(data["lien"],
+		data["lienId"]).call()
 else:
-    raise Exception("function not found")
+	raise Exception("function not found")
 
-signed_tx = w3.eth.account.sign_transaction(
-    unsent_tx.build_transaction(
-        {"from": account.address, "nonce": nonce, "gasPrice": w3.eth.gas_price}
-    ),
-    private_key=account.key,
-)
-tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-print("Transaction hash: ", tx_hash.hex())
-w3.eth.wait_for_transaction_receipt((tx_hash))
+if return_data is not None:
+	print(return_data)
+else:
+	signed_tx = w3.eth.account.sign_transaction(
+		unsent_tx.build_transaction(
+			{"from": account.address, "nonce": nonce, "gasPrice": w3.eth.gas_price}
+		),
+		private_key=account.key,
+	)
+	tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+	print("Transaction hash: ", tx_hash.hex())
+	w3.eth.wait_for_transaction_receipt((tx_hash))
