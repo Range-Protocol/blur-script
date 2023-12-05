250ddd4dc4911674625a2fc86f154a806de47daf
0xbateman <0xbateman@rangeprotocol.com>
2023-12-05 18:03:32 +0800

    add balance getter and array cleaner funcs

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from web3 import Web3
 from web3.middleware import construct_sign_and_send_raw_middleware
 
 whitelisted_functions = ["refinanceAuction", "startAuction", "seize", "getCurrentDebtByLien",
-	"getRefinancingAuctionRate"]
+	"getRefinancingAuctionRate", "getVaultBalance", "cleanup"]
+
+read_or_write = {
+	"refinanceAuction": "write",
+	"startAuction": "write",
+	"seize": "write",
+	"getCurrentDebtByLien": "read",
+	"getRefinancingAuctionRate": "read",
+	"getVaultBalance": "read",
+	"cleanup": "write"
+}
 
 function_name = sys.argv[1]
 if function_name not in whitelisted_functions:
@@ -14,18 +24,26 @@ if function_name not in whitelisted_functions:
 pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 rpc = {"mainnet": "https://eth.llamarpc.com", "localhost": "http://127.0.0.1:8545"}
 
-w3 = Web3(Web3.HTTPProvider(rpc["localhost"]))
+w3 = Web3(Web3.HTTPProvider(rpc["mainnet"]))
 account = w3.eth.account.from_key(pk)
 w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
 
 file_abi = open("RangeProtocolBlurVault.json")
 VAULT_ABI = json.load(file_abi)
-vault_address = "0x1Dbbf529D78d6507B0dd71F6c02f41138d828990"
+vault_address = "0xeD72A71161258FC3Dc31e57650E2b464c69f4dC1"
 vault_instance = w3.eth.contract(address=vault_address, abi=VAULT_ABI)
+
+file_blur_pool_abi = open("BlurPool.json")
+BLUR_POOL_ABI = json.load(file_blur_pool_abi)
+blur_pool_address = "0x0000000000A39bb272e79075ade125fd351887Ac"
+blur_pool_instance = w3.eth.contract(address=blur_pool_address, abi=BLUR_POOL_ABI)
+
 nonce = w3.eth.get_transaction_count(account.address)
 
-f = open(f"transaction_calldata/{function_name}.json")
-data = json.load(f)
+data = None
+if read_or_write[function_name] == "write":
+	f = open(f"transaction_calldata/{function_name}.json")
+	data = json.load(f)
 unsent_tx = None
 return_data = None
 
@@ -42,6 +60,10 @@ elif function_name == "getCurrentDebtByLien":
 elif function_name == "getRefinancingAuctionRate":
 	return_data = vault_instance.functions.getRefinancingAuctionRate(data["lien"],
 		data["lienId"]).call()
+elif function_name == "getVaultBalance":
+	return_data = blur_pool_instance.functions.balanceOf(vault_address).call() / 10 ** 18
+elif function_name == "cleanup":
+	unsent_tx = blur_pool_instance.functions.cleanUpLiensArray()
 else:
 	raise Exception("function not found")
 
